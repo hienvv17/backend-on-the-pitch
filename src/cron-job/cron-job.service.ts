28ef372d46a8244639570760d50eff481cb2460e
vohien1712 <vanhienvo01@gmail.com>
2025-05-19 02:56:13 +0000

    revert-cron

--- src/cron-job/cron-job.service.ts
+++ src/cron-job/cron-job.service.ts
@@ -208,7 +208,7 @@ export class CronJobService {
     // Generate loyalty vouchers for users who have made total amount bookings last month
   }
 
-  @Cron('*/2 * * * *')
+  @Cron('*/10 * * * *')
   async handleEveryTenMinCron() {
     const now = new Date();
     const fifteenMinutesAgo = new Date(now.getTime() - 10 * 60 * 1000);
